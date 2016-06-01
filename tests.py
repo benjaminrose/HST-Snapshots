fe35292ba4a29e81e545c2e9da9868edd1c2f723
Benjamin Rose <benjamin.rose@me.com>
2016-05-31 20:34:10 -0400

    update test for sdss defined galaxies
    It does not put the SN in the figures correctly.

--- tests.py
+++ tests.py
@@ -23,23 +23,37 @@ from astropy.wcs import WCS
 import ancillary
 import fractionalRank
 
-def correct_galaxy(n=25):
+def correct_galaxy(telescope, n=9):
 	"""
 	This saves visual images to see if defGalaxy is working.
 
 	# Parameters
+	telescope : string
+		use either 'hst' or 'sdss' to designate what galaxy definition you
+		want to test
+
 	n : int
 		The scaling of (a,b) because Source Extractor is stupid.
 	"""
 	currentTime = datetime.now()
 
 	# Get galaxy information -- from csv file
-	galaxies = Table.read('resources/hosts_25.csv', format='ascii.csv', header_start=2)
+	#todo(make this a simple system then the 5 or more if-else statements)
+	if telescope == 'hst':
+		galaxies = Table.read('resources/hosts_25.csv', format='ascii.csv', header_start=2)
+	elif telescope == 'sdss':
+		galaxies = Table.read('resources/sdss_hosts_2_test.csv', format='ascii.csv', header_start=2)
+	else:
+		raise ValueError('{} is unacceptable, use "hst" or "sdss" to represent the correct galaxy definition you want to test.'.format(telescope))
 	#todo(make the table work better, it is getting the first column name as '# SN' rather then 'SN')
 	galaxies['theta'].unit = u.radian
 
 	# Get Positions
-	positions = map(fractionalRank.get_SN_HST_coord, galaxies['# SN'].data)
+	if telescope == 'hst':
+		positions = map(fractionalRank.get_SN_HST_coord, galaxies['# SN'].data)
+	elif telescope == 'sdss':
+		positions = map(fractionalRank.get_SN_SDSS_coord, galaxies['# SN'].data)
+
 
 	# for each SN
 	for sn, position, x, y, a, b, theta in zip(galaxies['# SN'], 
@@ -47,14 +61,25 @@ def correct_galaxy(n=25):
 		galaxies['b'], galaxies['theta'].quantity):
 
 		# get scince data
-		hdu, data = ancillary.import_fits('data/HST - combined/SN{0}_combined.fits'.format(sn), extention=1)
+		if telescope == 'hst':
+			hdu, data = ancillary.import_fits('data/HST - combined/SN{0}_combined.fits'.format(sn), extention=1)
+		elif telescope == 'sdss':
+			if sn in [12928, 15171, 19023]:
+				raise Warning('SN{} does not exist for SDSS'.format(sn))
+			else:
+				hdu, data = ancillary.import_fits('data/SDSS - coadded/SN{0}-g.fits'.format(sn), extention=0)
 
 		# get WCS data & convert SN position to pixel
-		w = WCS(hdu[1].header)
+		if telescope == 'hst':
+			w = WCS(hdu[1].header)
+		elif telescope == 'sdss':
+			w = WCS(hdu[0].header)
 		x_sn, y_sn = w.all_world2pix(position.ra.to(u.deg).value, position.dec.to(u.deg).value, 1) #this should be better, do it all at once?
 		#all_world2pix gives non-whole numbers
 		x_sn, y_sn = round(x_sn), round(y_sn) #can do np.round (returns lists) or round (returns value)
 
+		print('SN : ', position.to_string('dms'))
+		print('SN pixel: ', x_sn, y_sn)
 		###### Plot ######
 		# figure out region to plot
 		center = (x, y)
@@ -68,9 +93,17 @@ def correct_galaxy(n=25):
 		ax = fig.add_subplot(111, projection=w)
 
 		# plot data
-		plt.imshow(data, origin='lower', cmap='cubehelix', clim=(0, 1))
+		if telescope == 'hst':
+			plt.imshow(data, origin='lower', cmap='cubehelix', clim=(0, 1))
+			plt.plot(x_sn, y_sn, marker='*', markersize=15, markerfacecolor='r', markeredgecolor='w')
+			plt.xlabel('RA')
+			plt.ylabel('Dec')
+		if telescope == 'sdss':
+			plt.imshow(data, origin='lower', cmap='cubehelix', clim=(5, 40))
+			plt.plot(y_sn, x_sn, marker='*', markersize=45, markerfacecolor='r', markeredgecolor='w')
+			plt.xlabel('Dec')
+			plt.ylabel('RA')
 		plt.colorbar()
-		plt.plot(x_sn, y_sn, marker='*', markersize=15, markerfacecolor='r', markeredgecolor='w')
 
 		# plot elipse
 		e = Ellipse(center, n*a, n*b, theta.to(u.deg).value)
@@ -81,16 +114,17 @@ def correct_galaxy(n=25):
 		# clip figure and add labels
 		plt.xlim(x_plot[0], x_plot[1])
 		plt.ylim(y_plot[0], y_plot[1]) 
-		plt.xlabel('RA')
-		plt.ylabel('Dec')
+
+		plt.show()
+		import sys; sys.exit()
 
 		#save figure
 		folder = 'test_results/correct_galaxy/'+currentTime.strftime("%Y-%m-%d %H:%M:%S")
 		if not path.exists(folder): makedirs(folder)
-		plt.savefig(folder+'/SN'+str(sn)+'_scaled'+str(n)+'.pdf')
+		plt.savefig(folder+'/'+telescope+'_SN'+str(sn)+'_scaled'+str(n)+'.pdf')
 
 		#close to same memory
 		plt.close(str(sn))
 
 if __name__ == "__main__":
-	correct_galaxy()
+	correct_galaxy('sdss')
